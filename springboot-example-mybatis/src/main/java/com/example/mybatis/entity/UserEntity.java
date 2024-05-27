c137cb4b439ac79c2747d7d208a01c26a913d29c
FlyHippo <whoarewe4u@protonmail.com>
2024-05-27 12:33:41 +0800

    add(mybatis): 完善有关mybatis整合的模块

---
+++ springboot-example-mybatis/src/main/java/com/example/mybatis/entity/UserEntity.java
@@ -0,0 +1,22 @@
+package com.example.mybatis.entity;
+
+import lombok.Data;
+
+@Data
+public class UserEntity {
+
+    private Long id;
+
+    private String name;
+
+    private String sex;
+
+    private Integer age;
+
+    private String email;
+
+    private String tel;
+
+    private String address;
+
+}
---
+++ springboot-example-mybatis/src/test/java/com/example/mybatis/mapper/UserMapperTest.java
@@ -0,0 +1,26 @@
+package com.example.mybatis.mapper;
+
+import com.example.mybatis.entity.UserEntity;
+import com.example.mybatis.mapper.UserMapperXml;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UserMapperTest {
+
+    @Autowired
+    private UserMapperXml userMapperXml;
+
+    @Test
+    public void testGetStudentById() {
+
+        List<UserEntity> all = userMapperXml.findAll();
+        all.forEach(System.out::println);
+    }
+}
